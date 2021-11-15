7bd2fcc4a741767207b68f5c9fc1118cdf08f24e
Erwan Jestin <erwan.jestin@myscript.com>
2021-11-15 21:54:14 +0100

    Remove commented code (js)

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,20 +9,6 @@
     $(this).toggleClass("active");
   });
 
-  // Smooth scrolling using jQuery easing
-  // $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-  //   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-  //     var target = $(this.hash);
-  //     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-  //     if (target.length) {
-  //       $('html, body').animate({
-  //         scrollTop: target.offset().top
-  //       }, 1000, "easeInOutExpo");
-  //       return false;
-  //     }
-  //   }
-  // });
-
   // Closes responsive menu when a scroll trigger link is clicked
   $('#sidebar-wrapper .js-scroll-trigger').click(function() {
     $("#sidebar-wrapper").removeClass("active");
